22be118692ace5b53084fe902aabbfa7e47dd9ee
Maxim Mozgovoy <djimmi.m@mail.ru>
2024-03-11 18:10:01 +0300

    Add selectionOfMaterials and SelectionOfEngineeringSystems, fix -plan

--- buildingConstruction/src/main/java/ru/sfedu/buildingconstruction/BuildingConstructionClient.java
+++ buildingConstruction/src/main/java/ru/sfedu/buildingconstruction/BuildingConstructionClient.java
@@ -41,19 +41,22 @@ public class BuildingConstructionClient {
     }
 
     public static void main(String[] args) throws Exception {
-        
+
         DataProvider provider;
+
+        String [] x = {"-plan", "House", "35", "4", "22", "1", "1 2"};
+//        String [] x = {"-worker", "12345g", "Max", "xxx", "89547854545", "-5012"};
+//        String [] x = {"-equipment", "12345g", "xxx", "-7373"};
+//        String [] x = {"-plan", "12345g", "xxx", "-7373"};
         
-        String [] x = {"DB"};
-        
+//-plan  [Building] [square] [numberOfFloors] [numberOfApartments] [client] [materials] [EngineeringSystems] 
+
         args = x;
-        
 
         if (args.length == 0) {
             log.debug("main [1]: данные не были получены");
             return;
         }
-
 
         String[] arguments = null;
 
@@ -75,7 +78,7 @@ public class BuildingConstructionClient {
                 arguments = Arrays.copyOfRange(args, 1, args.length);
                 ((DataBaseProvider) provider).createAllTables();
                 log.info("Выбран DB дата провайдер");
-                
+
             }
             default -> {
                 provider = new DataProviderCSV();
@@ -84,201 +87,183 @@ public class BuildingConstructionClient {
                 arguments = args;
             }
         }
-//
-//
-//        log.info("Дата провайдер установлен  " + provider.getClass().getSimpleName());
-//
-//        switch (arguments[0]) {
-//            case "-owner" -> {
-//
-//                if (arguments.length < 6) {
-//                    log.error("Данные введены некорректно");
-//                    return;
-//                }
-//                Client client = new Client();
-//                client.setId(arguments[1]);
-//                client.setName(arguments[2]);
-//
-//                if (EntityConfugurationUtil.phoneNumberIsCorrect(arguments[3])) {
-//                    client.setPhoneNumber(arguments[3]);
-//                } else {
-//                    return;
-//                }
-//
-//                if (EntityConfugurationUtil.emailIsCorrect(arguments[4])) {
-//                    client.setEmail(arguments[4]);
-//                } else {
-//                    return;
-//                }
-//
-//                if (EntityConfugurationUtil.passportIsCorrect(arguments[5])) {
-//                    client.setPassport(arguments[5]);
-//                } else {
-//                    return;
-//                }
-//
-//                try {
-//                    provider.addClient(client);
-//                    log.info("Запись " + client + " добавлена");
-//                } catch (IllegalArgumentException ex) {
-//                    log.error("Запись " + client + " уже создана");
-//                }
-//            }
-//
-//            case "-material" -> {
-//                if (arguments.length < 5) {
-//                    log.error("Данные введены некорректно");
-//                    return;
-//                }
-//                Material material = new Material();
-//                material.setId(arguments[1]);
-//                material.setName(arguments[2]);
-//
-//                if (EntityConfugurationUtil.numberIsPositive(Integer.parseInt(arguments[3]))) {
-//                    material.setPrice(Integer.parseInt(arguments[3]));
-//                } else {
-//                    return;
-//                }
-//                if (EntityConfugurationUtil.numberIsPositive(Double.parseDouble(arguments[4]))) {
-//                    material.setQuantityInStock(Double.parseDouble(arguments[4]));
-//                } else {
-//                    return;
-//                }
-//
-//                try {
-//                    provider.addMaterial(material);
-//                    log.info("Запись " + material + " добавлена");
-//                } catch (IllegalArgumentException ex) {
-//                    log.error("Запись " + material + " уже создана");
-//                }
-//
-//            }
-//            case "-worker" -> {
-//                if (arguments.length < 6) {
-//                    log.error("Данные введены некорректно");
-//                    return;
-//                }
-//                Worker worker = new Worker();
-//                worker.setId(arguments[1]);
-//                worker.setName(arguments[2]);
-//
-//                if (EntityConfugurationUtil.phoneNumberIsCorrect((arguments[3]))) {
-//                    worker.setPhoneNumber(arguments[3]);
-//                } else {
-//                    return;
-//                }
-//                if (EntityConfugurationUtil.numberIsPositive(Double.parseDouble(arguments[4]))) {
-//                    worker.setSalary(Double.parseDouble(arguments[4]));
-//                } else {
-//                    return;
-//                }
-//
-//                try {
-//                    provider.addWorker(worker);
-//                    log.info("Запись " + worker + " добавлена");
-//                } catch (IllegalArgumentException ex) {
-//                    log.error("Запись " + worker + " уже создана");
-//                }
-//            }
-//            case "-equipment" -> {
-//                if (arguments.length < 4) {
-//                    log.error("Данные введены некорректно");
-//                    return;
-//                }
-//                ConstructionEquipment equipment = new ConstructionEquipment();
-//                equipment.setId(arguments[1]);
-//                equipment.setName(arguments[2]);
-//
-//                if (EntityConfugurationUtil.numberIsPositive((Double.parseDouble(arguments[3])))) {
-//                    equipment.setPrice(Double.parseDouble(arguments[3]));
-//                } else {
-//                    return;
-//                }
-//
-//                try {
-//                    provider.addConstructionEquipment(equipment);
-//                    log.info("Запись " + equipment + " добавлена");
-//                } catch (IllegalArgumentException ex) {
-//                    log.error("Запись " + equipment + " уже создана");
-//                }
-//            }
-//            case "-plan" -> {
-//                if (arguments.length < 7) {
-//                    log.error("Данные введены некорректно");
-//                    return;
-//                }
-//
-//                Building building;
-//
-//                switch (arguments[1]) {
-//
-//                    case "ApartmentHouse" -> {
-//                        building = new ApartmentHouse();
-//                        ((ApartmentHouse) building).setNumberOfApartments(Integer.parseInt(arguments[3]));
-//                    }
-//                    case "House" -> {
-//                        building = new House();
-//                        ((House) building).setNumberOfRooms(Integer.parseInt(arguments[3]));
-//                    }
-//                    case "Garage" -> {
-//                        building = new Garage();
-//                        ((Garage) building).setNumberOfCars(Integer.parseInt(arguments[3]));
-//                    }
-//                    default -> {
-//                        log.error("Данные введены некорректно");
-//                        return;
-//                    }
-//                }
-//
-//                log.debug(building.getClass());
-//
-//                building.setSquare(Double.parseDouble(arguments[2]));
-//                building.setNumberOfFloors(Integer.parseInt(arguments[3]));
-//
-//                if (provider.getClient(arguments[5]).isEmpty()) {
-//                    log.error("Клиент не найден");
-//                }
-//                Client client = (Client) provider.getClient(arguments[5]).get();
-//
-//                List<Material> list = new ArrayList<>();
-//
-//                Arrays.stream(arguments[6].split(",")).forEach(el -> {
-//
-//                    try {
-//                        list.add(provider.getMaterial(el).get());
-//                    } catch (IOException ex) {
-//                        log.error(ex.getMessage());
-//                    } catch (NoSuchElementException x) {
-//                        log.error("материал не найден");
-//                        System.exit(0);
-//                    }
-//
-//                });
-//
-//                List<EngineeringSystem> systems = new ArrayList<>();
-//
-//                if (arguments.length < 8) {
-//                    try {
-//                        provider.preparationOfConstructionPlan(building, client, list, systems);
-//                    } catch (Exception ex) {
-//                        log.error("Невозможно добавить новую постройку");
-//                        System.exit(0);
-//                    }
-//                }
-//
-//                Arrays.stream(arguments[7].split(",")).forEach(el -> {
-//
-//                    systems.add(EngineeringSystem.valueOf(el));
-//
-//                });
-//
-//                try {
-//                    provider.preparationOfConstructionPlan(building, client, list, systems);
-//                } catch (Exception ex) {
-//                    log.error("Невозможно добавить новую постройку");
-//                    System.exit(0);
-//                }
-//            }
+
+        log.info("Дата провайдер установлен  " + provider.getClass().getSimpleName());
+
+        switch (arguments[0]) {
+            case "-owner" -> {
+
+                if (arguments.length < 6) {
+                    log.error("Данные введены некорректно");
+                    return;
+                }
+                Client client = new Client();
+                client.setId(arguments[1]);
+                client.setName(arguments[2]);
+
+                if (EntityConfugurationUtil.phoneNumberIsCorrect(arguments[3])) {
+                    client.setPhoneNumber(arguments[3]);
+                } else {
+                    return;
+                }
+
+                if (EntityConfugurationUtil.emailIsCorrect(arguments[4])) {
+                    client.setEmail(arguments[4]);
+                } else {
+                    return;
+                }
+
+                if (EntityConfugurationUtil.passportIsCorrect(arguments[5])) {
+                    client.setPassport(arguments[5]);
+                } else {
+                    return;
+                }
+
+                try {
+                    provider.addClient(client);
+                    log.info("Запись " + client + " добавлена");
+                } catch (IllegalArgumentException ex) {
+                    log.error("Запись " + client + " уже создана");
+                }
+            }
+
+            case "-material" -> {
+                if (arguments.length < 5) {
+                    log.error("Данные введены некорректно");
+                    return;
+                }
+                Material material = new Material();
+                material.setId(arguments[1]);
+                material.setName(arguments[2]);
+
+                if (EntityConfugurationUtil.numberIsPositive(Integer.parseInt(arguments[3]))) {
+                    material.setPrice(Integer.parseInt(arguments[3]));
+                } else {
+                    return;
+                }
+                if (EntityConfugurationUtil.numberIsPositive(Double.parseDouble(arguments[4]))) {
+                    material.setQuantityInStock(Double.parseDouble(arguments[4]));
+                } else {
+                    return;
+                }
+
+                try {
+                    provider.addMaterial(material);
+                    log.info("Запись " + material + " добавлена");
+                } catch (IllegalArgumentException ex) {
+                    log.error("Запись " + material + " уже создана");
+                }
+
+            }
+
+            case "-worker" -> {
+                if (arguments.length < 6) {
+                    log.error("Данные введены некорректно");
+                    return;
+                }
+                Worker worker = new Worker();
+                worker.setId(arguments[1]);
+                worker.setName(arguments[2]);
+                worker.setJobTitle(arguments[3]);
+
+                if (EntityConfugurationUtil.phoneNumberIsCorrect((arguments[4]))) {
+                    worker.setPhoneNumber(arguments[4]);
+                } else {
+                    return;
+                }
+                if (EntityConfugurationUtil.numberIsPositive(Double.parseDouble(arguments[5]))) {
+                    worker.setSalary(Double.parseDouble(arguments[5]));
+                } else {
+                    return;
+                }
+
+                try {
+                    provider.addWorker(worker);
+                    log.info("Запись " + worker + " добавлена");
+                } catch (IllegalArgumentException ex) {
+                    log.error("Запись " + worker + " уже создана");
+                }
+            }
+            case "-equipment" -> {
+                if (arguments.length < 4) {
+                    log.error("Данные введены некорректно");
+                    return;
+                }
+                ConstructionEquipment equipment = new ConstructionEquipment();
+                equipment.setId(arguments[1]);
+                equipment.setName(arguments[2]);
+
+                if (EntityConfugurationUtil.numberIsPositive((Double.parseDouble(arguments[3])))) {
+                    equipment.setPrice(Double.parseDouble(arguments[3]));
+                } else {
+                    return;
+                }
+
+                try {
+                    provider.addConstructionEquipment(equipment);
+                    log.info("Запись " + equipment + " добавлена");
+                } catch (IllegalArgumentException ex) {
+                    log.error("Запись " + equipment + " уже создана");
+                }
+            }
+
+//              java -jar buildingConstruction.jar [dataProvider] 
+//            -plan  [Building] [square] [numberOfFloors] [numberOfApartments] [client] [materials] [EngineeringSystems] 
+            case "-plan" -> {
+                if (arguments.length < 7) {
+                    log.error("Данные введены некорректно");
+                    return;
+                }
+
+                Building building;
+
+                switch (arguments[1]) {
+
+                    case "ApartmentHouse" -> {
+                        building = new ApartmentHouse();
+                        ((ApartmentHouse) building).setNumberOfApartments(Integer.parseInt(arguments[4]));
+                    }
+                    case "House" -> {
+                        building = new House();
+                        ((House) building).setNumberOfRooms(Integer.parseInt(arguments[4]));
+                    }
+                    case "Garage" -> {
+                        building = new Garage();
+                        ((Garage) building).setNumberOfCars(Integer.parseInt(arguments[4]));
+                    }
+                    default -> {
+                        log.error("Данные введены некорректно");
+                        return;
+                    }
+                }
+
+                log.debug(building.getClass());
+
+                building.setSquare(Double.parseDouble(arguments[2]));
+                building.setNumberOfFloors(Integer.parseInt(arguments[3]));
+
+                if (provider.getClient(arguments[5]).isEmpty()) {
+                    log.error("Клиент не найден");
+                }
+                Client client = (Client) provider.getClient(arguments[5]).get();
+
+                List<Material> list = provider.selectionOfMaterials(arguments[6]);
+
+                List<EngineeringSystem> systems = null;
+
+                if (arguments.length > 7) {
+                    systems = provider.selectionOfEngineeringSystems(arguments[7]);
+                }
+
+                try {
+                    provider.preparationOfConstructionPlan(building, client, list, systems);
+                } catch (Exception ex) {
+                    log.error("Невозможно добавить новую постройку");
+                    return;
+                }
+
+            }
+        }
 //
 //            case "-build" -> {
 //                if (arguments.length < 3) {
--- buildingConstruction/src/main/java/ru/sfedu/buildingconstruction/api/DataProvider.java
+++ buildingConstruction/src/main/java/ru/sfedu/buildingconstruction/api/DataProvider.java
@@ -1,46 +1,99 @@
-
 package ru.sfedu.buildingconstruction.api;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import ru.sfedu.buildingconstruction.model.*;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author maksim
  */
 public interface DataProvider {
-    
+
+    static Logger log = Logger.getLogger(DataProvider.class);
+
+    public default List<Material> selectionOfMaterials(String str) {
+
+        List<Material> list = new ArrayList<>();
+
+        Arrays.stream(str.split(" ")).forEach(el -> {
+
+            try {
+                list.add(getMaterial(el).get());
+            } catch (IOException ex) {
+                log.error(ex.getMessage());
+            } catch (NoSuchElementException ex) {
+                log.error("материал не найден");
+                System.exit(0);
+            }
+
+        });
+        return list;
+    }
+
+    public default List<EngineeringSystem> selectionOfEngineeringSystems(String str) {
+
+        List<EngineeringSystem> list = new ArrayList<>();
+
+        Arrays.stream(str.split(" ")).forEach(el -> {
+
+            list.add(EngineeringSystem.valueOf(el));
+        });
+
+        return list;
+    }
+
     public void preparationOfConstructionPlan(Building building, Client client, List<Material> materials, List<EngineeringSystem> systems) throws Exception;
+
     public void preparationForBuilding(Building building);
+
     public void calculationOfTheTotalCost(Building building);
-    
+
     public void addWorker(Worker worker) throws IOException;
+
     public void addConstructionEquipment(ConstructionEquipment constructionEquipment) throws IOException;
+
     public void addMaterial(Material material) throws IOException;
+
     public void addClient(Client client) throws IOException;
+
     public void addBuilding(Building building) throws IOException;
-    
+
     public void deleteWorker(String id) throws IOException;
+
     public void deleteConstructionEquipment(String id) throws IOException;
+
     public void deleteMaterial(String id) throws IOException;
+
     public void deleteClient(String id) throws IOException;
+
     public void deleteBuilding(String id, Class<? extends Building> clazz) throws IOException;
-    
+
     public Optional<Worker> getWorker(String id) throws IOException;
+
     public Optional<ConstructionEquipment> getConstructionEquipment(String id) throws IOException;
+
     public Optional<Material> getMaterial(String id) throws IOException;
+
     public Optional<Client> getClient(String id) throws IOException;
+
     public Optional<? extends Building> getBuilding(String id, Class<? extends Building> clazz) throws IOException;
-    
+
     public <T> List<T> getAllRecords(Class clazz, String path) throws IOException;
-    
+
     public void updateWorker(String id, Worker worker) throws IOException;
+
     public void updateConstructionEquipment(String id, ConstructionEquipment constructionEquipment) throws IOException;
+
     public void updateMaterial(String id, Material material) throws IOException;
+
     public void updateClient(String id, Client client) throws IOException;
+
     public void updateBuilding(String id, Building building) throws IOException;
-    
-    
+
 }
